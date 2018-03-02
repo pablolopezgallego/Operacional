e7a8676f6555b9c8766ad01ea5bd08c17ce902fe
pablolopezgallego <36295104+pablolopezgallego@users.noreply.github.com>
2018-03-02 14:13:55 +0100

    Operacional

--- Operacional.sh
+++ Operacional.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
-(cd /root/borrar3/Operacional/; sbt package)
-spark-submit --class Operacional --master yarn-cluster --packages it.nerdammer.bigdata:spark-hbase-connector_2.10:1.0.3,org.apache.spark:spark-streaming-kafka_2.10:1.6.2 --conf spark.yarn.appMasterEnv.ENTORNO=PRODUCCION --executor-memory 2G --driver-memory 4G --num-executors 4 --executor-cores 1 --driver-cores 4 /root/borrar3/Operacional/target/scala-2.10/spark_2.10-1.0.jar
+
+if [[ $# -eq 0 ]] ; then
+    echo 'necesitas especificar el nombre de cliente'
+    exit 1
+fi
+
+(cd /root/cliente$1/spark/; sbt package)
+spark-submit --class OperacionalHBase --master yarn-cluster --packages it.nerdammer.bigdata:spark-hbase-connector_2.10:1.0.3,org.apache.spark:spark-streaming-kafka_2.10:1.6.2 --conf spark.yarn.appMasterEnv.ENTORNO=PRODUCCION --executor-memory 2G --driver-memory 4G --num-executors 4 --executor-cores 1 --driver-cores 4 /root/cliente$1/spark/target/scala-2.10/spark_2.10-1.0.jar operacional$1 /$1/resultados/adn/
